a63c78703228ddbbf3c3bebb6353c49d99bd471f
Anna <s1601847@sms.ed.ac.uk>
2019-03-16 23:05:20 +0000

    i'm a dumbass

--- names.py
+++ names.py
@@ -9,16 +9,16 @@ def declaration_check(list):
     is_declared=["" for line in list]
     for i,line in enumerate(list):
             if isinstance(line[0], Variable) and isinstance(line[1], ISA):
-                if line[0] in is_declared:
+                if line[0].label in is_declared:
                     error()
                 else:
-                    is_declared[list.index[line]]=line[0]
+                    is_declared[list.index[line]]=line[0].label
 
 
 
 
     for line in list:
-        if line[0] in is_declared[:(list.index[line]+1)]:
+        if line[0].label in is_declared[:(list.index[line]+1)]:
             pass
         else:
             error()
