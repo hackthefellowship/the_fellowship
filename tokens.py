be547f57f841ca748ce3f5876192a09941446382
ruth turpie <ruthturpie@c160138.wlan.net.ed.ac.uk>
2019-03-16 16:03:32 +0000

    tokens

---
+++ tokens.py
@@ -0,0 +1,103 @@
+from error import *
+
+filein = open("actions.txt","r")
+lines = filein.readlines()
+filein.close()
+
+actions = []
+interactions = []
+races = []
+
+for line in lines:
+    line = line.strip()
+    print(line.split(":"))
+    name, lst = line.split(":")
+    lst = lst.split(",")
+    if name == "actions":
+        actions = [l for l in lst]
+    if name == "interactions":
+        interactions = [l for l in lst]
+    if name == "races":
+        races = [l for l in lst]
+       
+    
+class Action:
+
+    def __init__(self, label):
+
+        self.valid = self.check_valid(label)
+        self.label = label
+
+    def check_valid(self, label):
+
+        if label in actions:
+
+            return True
+
+        else:
+
+            return False
+        
+
+class Interaction:
+
+    def __init__(self, label):
+
+        self.valid = self.check_valid(label)
+        self.label = label
+
+    def check_valid(self,label):
+
+        if label in interactions:
+
+            return True
+
+        else:
+
+            return False
+
+
+class Type:
+    
+    def __init__(self, label):
+
+        self.valid = self.check_valid(label)
+        self.label = label
+
+    def check_valid(self):
+
+        def check_valid(self,label):
+
+        if label in races:
+
+            return True
+
+        else:
+
+            return False
+
+class Possesion:
+
+    def __init__(self, label):
+
+        self.valid = self.check_valid(label)
+        self.label = label
+
+    def check_valid(self):
+
+        def check_valid(self,label):
+
+        if label in possesions:
+
+            return True
+
+        else:
+
+            return False
+
+class Variable:
+
+    def __init__(self,label):
+
+        self.valid = True
+        self.label = label
