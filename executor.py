d4bcbb70bebf35b180d9c6e4fd79740bf586d436
Alastair <ally@hvitt.co.uk>
2019-03-17 00:49:19 +0000

    what the what

--- executor.py
+++ executor.py
@@ -1,9 +1,26 @@
-
+from tokens import *
 
 def execute(characters, tokens):
-    for line in tokens:
+    i=0
+    while i < len(tokens):
+        line = tokens[i]
         char = line[0].label
         for c in characters:
             if c.name == char:
                 char = c
         print (char)
+
+        if len(line) == 2:
+            if line[1].label == "naps":
+                char.naps()
+            elif line[1].label == "sleeps deeply":
+                char.sleeps_deeply()
+            elif line[1].label == "is amazed":
+                char.is_amazed()
+            elif line[1].label == "ponders":
+                char.ponders()
+        
+        if len(line) == 3:
+            if isinstance(line[1], Move):
+                char.travels(line[2].label)
+        i += 1
